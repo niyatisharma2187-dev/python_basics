24aa1f6bfb23ecedf62abc0fbbf1dc2f5ac0b962
niyatisharma2187-dev <niyatisharma2187@gmail.com>
2026-05-29 14:09:20 +0530

    Update guessNo.py

--- guessNo.py
+++ guessNo.py
@@ -9,4 +9,4 @@ while(a != n):
         print("lower number please!!")
     else:
         print("Higher number please!!")
-print(f"Congratulations you are assumed the right number which is:{guesses}")
+print(f"Congratulations you are assumed the right number which is:{n} on attemps {guesses}")
