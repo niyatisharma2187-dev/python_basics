99b9bc459f3547f61f196ca81351755013f1994d
niyatisharma2187-dev <niyatisharma2187@gmail.com>
2026-05-29 14:10:02 +0530

    Fix guess count initialization and increment

--- guessNo.py
+++ guessNo.py
@@ -1,12 +1,12 @@
 import random
 n=random.randint(1,100)
 a = -1
-guesses = 0
+guesses = 1
 while(a != n):
-    guesses +=1
     a=int(input("Enter the number: "))
     if(a>n):
         print("lower number please!!")
     else:
         print("Higher number please!!")
+    guesses +=1
 print(f"Congratulations you are assumed the right number which is:{n} on attemps {guesses}")
